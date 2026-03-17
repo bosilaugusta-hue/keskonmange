200da0c103f0f8bc3007bbbc725522f008fec172
AUDE CHARRIER <charrier.aude@gmail.com>
2026-03-17 12:40:06 +0100

    carousel js finished

--- homepage/home.js
+++ homepage/home.js
@@ -9,30 +9,11 @@ const padding = 1 * 16 /*padding=1rem*/
 const gap = 2 * 16; /*gap=2rem*/
 const boxCardWidth = numberOfCards[0].offsetWidth + padding + gap;
 const minIndex = 0;
-const maxIndex = numberOfCards.length - 2; /*ne peut pas dépasser la dernière box-card, il en restera tjs 2 affichées*/
+const maxIndex = numberOfCards.length - 2; /*il restera tjs 2 box-card affichées*/
 
 const firstDot = document.getElementById('firstDot');
 const secondDot = document.getElementById('secondDot');
 const thirdDot = document.getElementById('thirdDot');
-
-function moveCardsRight() {
-    if (index < maxIndex) {
-        index++;
-        divCards.style.transform = `translateX(-${index * boxCardWidth}px)`;
-        console.log(index);
-        return true;
-    } else {
-        return false
-    }
-};
-
-function moveCardsLeft() {
-    if (index > minIndex) {
-        index--;
-        divCards.style.transform = `translateX(-${index * boxCardWidth}px)`;
-        console.log(index);
-    }
-};
 
 function removeClassActive(dot) {
     if (dot.classList.contains("active")) {
@@ -40,31 +21,67 @@ function removeClassActive(dot) {
     }
 };
 
+function addClassActive() {
+    switch (index) {
+        case 0: firstDot.classList.add("active");
+            removeClassActive(secondDot);
+            removeClassActive(thirdDot);
+            break;
+        case 1: secondDot.classList.add("active");
+            removeClassActive(firstDot);
+            removeClassActive(thirdDot);
+            break;
+        case 2: thirdDot.classList.add("active");
+            removeClassActive(firstDot);
+            removeClassActive(secondDot);
+            break;
+    };
+};
+
+function moveCards() {
+    divCards.style.transform = `translateX(-${index * boxCardWidth}px)`;
+    addClassActive();
+};
+
+function goToIndex(newIndex) {
+    index = newIndex;
+};
+
 btnNext.addEventListener('click', () => {
-    moveCardsRight();
+    if (index < maxIndex) {
+        index++;
+        moveCards();
+    } else {
+        return
+    }
 });
 
 btnPrev.addEventListener('click', () => {
-    moveCardsLeft();
+    if (index > minIndex) {
+        index--;
+        moveCards();
+    } else {
+        return
+    }
 });
 
-
 firstDot.addEventListener('click', () => {
-    moveCardsLeft();
-    firstDot.classList.add("active");
-    removeClassActive(secondDot);
-    removeClassActive(thirdDot);
+    goToIndex(0);
+    moveCards();
+});
+
+secondDot.addEventListener('click', () => {
+    if (index === 0 || index === 2) {
+        goToIndex(1);
+        moveCards();
+    } else {
+        return
+    }
 });
 
 thirdDot.addEventListener('click', () => {
-    moveCardsRight();
-    let booleen = moveCardsRight();
-    if (booleen === true) {
-        thirdDot.classList.add("active");
-    }
-})
+    goToIndex(2);
+    moveCards();
+});
 
-//il faut que le first et le third bougent de 2 crans d'un coup
-//il faut que les prev et next add les active correspondant (et remove les autres)
-//next next --> third : rien ok
-//prev --> third bouge mais pas active
+
