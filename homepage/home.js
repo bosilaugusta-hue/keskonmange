84aad5b23c68a30eae4c9a874df62635441c5c55
AUDE CHARRIER <charrier.aude@gmail.com>
2026-03-17 22:34:21 +0100

    carousel alignment issues fixed - carousel js improved

--- homepage/home.js
+++ homepage/home.js
@@ -1,87 +1,69 @@
 const divCards = document.querySelector('.cards');
 const btnPrev = document.getElementById('prev');
 const btnNext = document.getElementById('next');
-
-let index = 0;
+const allDots = document.getElementsByClassName('dot');
 
 const numberOfCards = document.querySelectorAll(".box-card");
-const padding = 1 * 16 /*padding=1rem*/
-const gap = 2 * 16; /*gap=2rem*/
-const boxCardWidth = numberOfCards[0].offsetWidth + padding + gap;
-const minIndex = 0;
-const maxIndex = numberOfCards.length - 2; /*il restera tjs 2 box-card affichées*/
+const padding = 1 * 16 /*padding-côté=1rem*/
+const demiGap = 1 * 16; /*gap=2rem*/
+const boxCardWidth = numberOfCards[0].offsetWidth + padding + demiGap;
+/*numberOfCards[0].offsetWidth : je vais chercher la largeur d'une box-card*/
+/*je décale de padding et demi-gap = 1+1 = 2rem
+puis il faut décaler de demi-gap restant et demi-gap suivant= 1+1 = 2rem*/
 
-const firstDot = document.getElementById('firstDot');
-const secondDot = document.getElementById('secondDot');
-const thirdDot = document.getElementById('thirdDot');
+let position = 0;
+const minPosition = 0;
+const maxPosition = numberOfCards.length - 2;
+/*il restera tjs 2 box-card affichées*/
 
-function removeClassActive(dot) {
-    if (dot.classList.contains("active")) {
-        dot.classList.remove("active")
-    }
-};
+/************************************************************/
 
-function addClassActive() {
-    switch (index) {
-        case 0: firstDot.classList.add("active");
-            removeClassActive(secondDot);
-            removeClassActive(thirdDot);
-            break;
-        case 1: secondDot.classList.add("active");
-            removeClassActive(firstDot);
-            removeClassActive(thirdDot);
-            break;
-        case 2: thirdDot.classList.add("active");
-            removeClassActive(firstDot);
-            removeClassActive(secondDot);
-            break;
-    };
+function goToPosition(newPosition) {
+    position = newPosition;
 };
 
 function moveCards() {
-    divCards.style.transform = `translateX(-${index * boxCardWidth}px)`;
-    addClassActive();
+    divCards.style.transform = `translateX(-${position * boxCardWidth}px)`;
 };
 
-function goToIndex(newIndex) {
-    index = newIndex;
+function updateClassActive(index) {
+    for (let j = 0; j < allDots.length; j++) {
+        if (allDots[j].classList.contains("active")) {
+            allDots[j].classList.remove("active");
+        }
+    };
+    allDots[index].classList.add("active");
 };
+
+/*********************************************************/
 
 btnNext.addEventListener('click', () => {
-    if (index < maxIndex) {
-        index++;
+    if (position < maxPosition) {
+        position++;
         moveCards();
+        updateClassActive(position);
     } else {
         return
     }
 });
 
 btnPrev.addEventListener('click', () => {
-    if (index > minIndex) {
-        index--;
+    if (position > minPosition) {
+        position--;
         moveCards();
+        updateClassActive(position);
     } else {
         return
     }
 });
 
-firstDot.addEventListener('click', () => {
-    goToIndex(0);
-    moveCards();
-});
-
-secondDot.addEventListener('click', () => {
-    if (index === 0 || index === 2) {
-        goToIndex(1);
+for (let i = 0; i < allDots.length; i++) {
+    allDots[i].addEventListener("click", () => {
+        goToPosition(i);
         moveCards();
-    } else {
-        return
+        updateClassActive(i);
     }
-});
-
-thirdDot.addEventListener('click', () => {
-    goToIndex(2);
-    moveCards();
-});
+    )
+};
 
 
