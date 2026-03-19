43b6c821343859351a7663dc98cb8c181143a9f5
Leo Fleury <leofleury72@gmail.com>
2026-03-19 17:48:50 +0100

    add last files and add js ans update css and html

--- comunity/comunity.js
+++ comunity/comunity.js
@@ -1,1 +1,20 @@
+const checkboxes = document.querySelectorAll('form input[type="checkbox"]');
+  const articles = document.querySelectorAll('.main-article');
 
+  function filter() {
+    const active = [...checkboxes]
+      .filter(cb => cb.checked)
+      .map(cb => cb.value);
+
+    if (active.length === 0) {
+      articles.forEach(a => a.hidden = false);
+      return;
+    }
+
+    articles.forEach(article => {
+      const categories = article.dataset.category.split(' ');
+      article.hidden = !active.some(val => categories.includes(val));
+    });
+  }
+
+  checkboxes.forEach(cb => cb.addEventListener('change', filter));
